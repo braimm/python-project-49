9b777a7dcec58aace120164e34f23c44db7ccc62
Dmitriy G <llogginn@gmail.com>
2024-01-07 19:26:08 +0500

    even_v1

---
+++ brain_games/games/even_game.py
@@ -0,0 +1,31 @@
+import random
+import prompt
+from importlib import reload
+from brain_games import cli
+
+
+
+def even_game():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}')
+    # name = cli.welcome_user()
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    for i in range(3):
+        number = random.randint(1, 100)
+        print(f'Question: {number}')
+        answer = prompt.string('Your answer: ')
+        mod = (number % 2)
+        if ((mod == 0) and (answer == 'yes')) or ((mod != 0) and (answer == 'no')):
+            print('Correct!')
+        else:
+            if mod == 0:
+                print(f"'{answer}' is wrong answer ;(. Correct answer was 'yes'.")
+            else:
+                print(f"'{answer}' is wrong answer ;(. Correct answer was 'no'.")
+            print(f"Let's try again, {name}!")
+            return
+    print(f"Congratulations, {name}!")
+    
+
+even_game()
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# from brain_games import cli
+from brain_games.games import even_game
+
+
+def main():
+    even_game()
+
+
+if __name__ == '__main__':
+    main()
